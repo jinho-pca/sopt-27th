86a7cbbd80bae7bc2dc90d1a0a9c2c5a42aa446a
jinho-pca <jinho_pca@naver.com>
2020-10-19 21:05:25 +0900

    [create] calculator, main file

---
+++ homework/homework2/LEVEL2/calculator.js
@@ -0,0 +1,11 @@
+const add = (x, y) => x + y;
+const substract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const Divide = (x, y) => x / y;
+
+module.exports = {
+    add,
+    substract,
+    multiply,
+    Divide
+}
---
+++ homework/homework2/LEVEL2/main.js
@@ -0,0 +1,6 @@
+const calc = require('./calculator');
+
+console.log(calc.add(2, 1));
+console.log(calc.substract(2, 1));
+console.log(calc.multiply(2, 1));
+console.log(calc.Divide(2, 1));
