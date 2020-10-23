894e5eb0298c051f5b4aeb6c249892b81def6401
jinho-pca <jinho_pca@naver.com>
2020-10-23 18:40:24 +0900

    [create] hw level3 file

---
+++ homework/homework2/LEVEL3/crypto.js
@@ -0,0 +1,14 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+const pw = '96695412';
+const fileName = 'CryptedPW';
+
+crypto.randomBytes(64, (err, buf) => {
+  const salt = buf.toString('base64');
+  crypto.pbkdf2(pw, salt, 100000, 64, 'sha512', (err, key) => {
+    fs.writeFile(`${fileName}.txt`, key, () => {
+      console.log(`file[${fileName}] write complete!`);
+    });
+  });
+});
